67dc3d272c42b0b8b444331daa759598088438dc
YWHo <YWHo@users.noreply.github.com>
2018-03-15 13:23:18 +1300

    enable ejs view engine

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 
-app.get("/", function(req, res) {
-    res.send("this will be the landing page soon!")
+app.listen(port, function() {
+    console.log("The YelpCamp Server Has Started!");
 })
 
-app.listen(port, function() {
-    console.log("The YelpCamp Server Has Started!")
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("landing");
 })
 
