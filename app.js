9820163e221ee87af5bb87cffabe208bdcf8d77c
YWHo <YWHo@users.noreply.github.com>
2018-03-15 15:03:17 +1300

    add a form and post route to submit a new campground

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser")
 const port = process.env.PORT || 3000
 
 app.listen(port, function() {
     console.log("The YelpCamp Server Has Started!")
 })
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs")
 
@@ -12,13 +15,25 @@ app.get("/", function(req, res) {
     res.render("landing")
 });
 
-app.get("/campgrounds", function(req, res) {
-    let campgrounds = [
-        {name: "Salmon Creek", image: "https://pixabay.com/get/eb37b60e20f6093ed1584d05fb1d4e97e07ee3d21cac104497f1c17ea7e9b1b8_340.jpg"},
-        {name: "Granite Hill", image: "https://pixabay.com/get/eb31b00e28f2083ed1584d05fb1d4e97e07ee3d21cac104497f1c17ea7e9b1b8_340.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e830b50c2bf5023ed1584d05fb1d4e97e07ee3d21cac104497f1c17fa4eeb6b0_340.jpg"}
-    ]
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://pixabay.com/get/eb37b60e20f6093ed1584d05fb1d4e97e07ee3d21cac104497f1c17ea7e9b1b8_340.jpg"},
+    {name: "Granite Hill", image: "https://pixabay.com/get/eb31b00e28f2083ed1584d05fb1d4e97e07ee3d21cac104497f1c17ea7e9b1b8_340.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e830b50c2bf5023ed1584d05fb1d4e97e07ee3d21cac104497f1c17fa4eeb6b0_340.jpg"}
+]
 
+app.get("/campgrounds", function(req, res) {
     res.render("campgrounds", {campgrounds: campgrounds});
+})
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs")
+})
+
+app.post("/campgrounds", function(req, res) {
+    let name = req.body.name
+    let image = req.body.image
+    let newCampground = {name, image}
+    campgrounds.push(newCampground)
+    res.redirect("/campgrounds")
 
 })
